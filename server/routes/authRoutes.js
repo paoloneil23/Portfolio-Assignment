f68e0f3daab7b25ae88d70c6e9cf05c2adcf0e5b
Paolo Caparas <caparaspaolo23@gmail.com>
2025-08-03 01:22:21 -0400

    more fixes part 3

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,8 +6,4 @@ const router = express.Router();
 router.post('/signin', signin);
 router.get('/signout', signout);
 
-router.get('/test', (req, res) => {
-  res.send('Auth route is working');
-});
-
 export default router;
