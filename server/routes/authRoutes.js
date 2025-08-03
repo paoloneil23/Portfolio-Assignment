5135a9790ad0f348cd5fa2acd6651ec1dac0cadc
Paolo Caparas <caparaspaolo23@gmail.com>
2025-08-03 00:09:27 -0400

    debuggin test

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,4 +6,8 @@ const router = express.Router();
 router.post('/signin', signin);
 router.get('/signout', signout);
 
+router.get('/test', (req, res) => {
+  res.send('Auth route is working');
+});
+
 export default router;
